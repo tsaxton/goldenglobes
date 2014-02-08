af9d533da0c4439c8aaec77132a80d80c4749567
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-02-08 16:01:03 -0600

    Pulling out winning actors/movies decently well

--- selection.py
+++ selection.py
@@ -3,19 +3,33 @@ import random
 import re
 
 def removeRT(tweet):
-    print re.split('RT\ \@\S+:', tweet,1)[1]
+    t = re.split('RT\s\@\S+\:\s', tweet,1)
+    if len(t)>1:
+    	return t[1]
+    else:
+    	return t[0]
 
 with open('goldenglobes.json', 'r') as f:
     tweets = map(json.loads, f)
 
 for t in tweets:
-	removeRT(t)
+	removeRT(t['text'])
 
-'''h = re.compile('.+.+wins.+.+for.+.+')
+h = re.compile('.+.+wins.+.+for.+.+')
 printed=[]
+winners={}
 for s in tweets:
-	if h.match(s['text']):
-		if s['text'] not in printed:
-		    printed.append(s['text'])
+	t = removeRT(s['text'])
+	if h.match(t):
+		#if t not in printed:
+		    #printed.append(t)
 		    #print re.sub(r'[^\x00-\x7F]+',' ', s['text'])
-		    print re.split('wins',s['text'])'''
+		    words = re.split('wins',t)
+		    if words[0] in winners.keys():
+		        winners[words[0]]+=1
+		    else:
+		    	winners[words[0]]=1
+
+for key in winners:
+	if winners[key] > 10:
+	    print key+': '+str(winners[key])
