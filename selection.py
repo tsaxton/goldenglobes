7a8680f0ee5ece4580a4768d4f960c06ce00d2a3
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-02-20 17:36:21 -0600

    Still trying to deal with presenters

--- selection.py
+++ selection.py
@@ -6,6 +6,8 @@ from tfidf import *
 import numpy
 import sys
 from alchemyapi import *
+from operator import itemgetter
+import pprint
 
 def removeRT(tweet):
     ''' Removes the RT @handle: rom the tweet.
@@ -193,13 +195,16 @@ def getHosts(tweets):
 # Imperatives Begin Here
 with open('goldenglobes.json', 'r') as f:
     tweets = map(json.loads, f)
+tweets = sorted(tweets, key=itemgetter('created_at')) 
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(tweets)
 cleanTweets = []
 for t in tweets:
 	cleanTweets.append(removeRT(t['text']))
 
 
-'''
-# 1. Find the names of the hosts
+
+'''# 1. Find the names of the hosts
 print getHosts(cleanTweets)
 # 2. For each award, find the name of the winner.
 results = winners(cleanTweets)
@@ -213,48 +218,75 @@ for n in nominees:
         print person
     print "\n"'''
 
-p = re.compile('.+.+present.+.+')
+presenters = ['Jessica Alba', 'Kiefer Sutherland', 'Aziz Ansari', 'Jason Bateman', 'Christian Bale', 'Kristen Bell', 'John Krasinski', 'Halle Berry', 'Josh Brolin', 'Don Cheadle', 'Eva Longoria', 'Bill Clinton', 'George Clooney', 'Sacha Baron Cohen', 'Bradley Cooper', 'Kate Hudson', 'Rosario Dawson', 'Robert Downey', 'Jimmy Fallon', 'Jay Leno', 'Will Ferrell', 'Kristen Wiig', 'Nathan Fillion', 'Lea Michele', 'Megan Fox', 'Jonah Hill', 'Jamie Foxx', 'Jennifer Garner', 'John Goodman', 'Tony Mendez', 'Salma Hayek', 'Paul Rudd', 'Dustin Hoffman', 'Jeremy Irons', 'Lucy Liu', 'Debra Messing', 'Jennifer Lopez', 'Jason Statham', 'Robert Pattinson', 'Amanda Seyfried', 'Dennis Quaid', 'Kerry Washington', 'Jeremy Renner', 'Julia Roberts', 'Liev Schreiber', 'Arnold Schwarzenegger', 'Sylvester Stalone', 'Catherine Zeta-Jones']
+
+tweetsAbout = {}
+for p in presenters:
+	tweetsAbout[p] = []
+
+for t in cleanTweets:
+	t = t.encode('utf-8').lower()
+	for p in presenters:
+		if p.lower() in t:
+			tweetsAbout[p].append(t)
+
+for p in tweetsAbout:
+	print "NEW CELEBRITY " + p.upper() + " WITH " + str(len(tweetsAbout[p])) + " TWEETS:"
+	for t in tweetsAbout[p]:
+		print t
+	print "\n\n"
+'''p = re.compile('.+.+present.+.+')
 query = ''
 alchemy = AlchemyAPI()
 entities = []
+matches = []
+matchBin = []
+binNumber = 0
+i = 0
 for t in cleanTweets:
 	if p.match(t):
-		#t.encode('utf8', 'ignore')
-		#urllib.quote(t)
+		matches.append(t.lower().encode('utf-8'))
+		matchBin.append(binNumber)
 		query += t.encode('utf-8') + ' '
 		if sys.getsizeof(query)>153000:
 			response = alchemy.entities('text',query,{})
 			if response['status'] == 'OK':
 				entities.extend(response['entities'])
 			query = ''
+	i += 1
+	if i == 5000:
+		binNumber += 1
+		i = 0
+presenters = {}
 for e in entities:
 	if e['type'] == 'Person':
 		eWords = e['text'].split(' ')
 		if len(eWords) >= 2 and len(eWords) <= 4 and len(eWords[0]) > 2 and eWords[0][0].isupper():
-			print e['text']
+			presenters[str(e['text'])] = {}
+print presenters
+g = re.compile('.+.+\sgoes\sto.+.+')
+h = re.compile('.+.+wins.+.+for.+.+')
+ret = {}'''
+'''for t in cleanTweets
+	t = t.encode('utf-8')
+	if h.match(t):
+		award = re.split('for\s',t)
+		for phrase in award:
+			q = phrase.lower().lstrip().rstrip()
+			if re.match('^best', q):
+				if len(q.split(" ")) > 6:
+					continue
+				if "#" in q:
+					continue
+			    # q is now award name
+	elif g.match(t):
+	    words = re.split('\sgoes\sto', t)
+	    if re.match('^best', words[0].lower().lstrip()):
+	    	category = words[0].lower().lstrip().rstrip()
+	    	if len(category.split(" ")) > 6:
+	    		continue
+	    	if "#" in q:
+	    		continue
+            # category is now award name'''
 
-# the following attempt at doing tf idf on the tweet corpus using NLTK takes too long
-# going to try using Alchemy API to see if that does any better
-# first want to try and extract the hosts
-'''vocabulary = []
-docs = []
-all_tips = []
-for i in range(len(cleanTweets)):
-	tip = cleanTweets[i]
-	tokens = tokenizer.tokenize(tip.lower())
-	 
-	bi_tokens = bigrams(tokens)
-	tri_tokens = trigrams(tokens)
 
-	for token in tokens:
-		if token in stopwords:
-			tokens.remove(token)
-
-	tokens.extend(bi_tokens)
-	tokens.extend(tri_tokens)
-	vocabulary.append(tokens) # want to append rather than extend to keep the tweets separate
-	docs.append({'freq': {}, 'tf': {}, 'idf': {},'tf-idf': {}, 'tokens': tokens})
-
-	for token in tokens:
-		docs[i]['freq'][token] = freq(token, tokens)
-		docs[i]['tf'][token] = tf(token, tokens)'''
